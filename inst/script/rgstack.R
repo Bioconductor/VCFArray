4fdc76114a91cfcfe7162f54e9c62ca4d75d3fca
qian <qliu7@buffalo.edu>
2018-12-06 12:37:17 -0500

    removing the script in generating rgstack example

--- inst/script/rgstack.R
+++
@@ -1,9 +0,0 @@
-extdata <- system.file(package="GenomicFiles", "extdata")
-files <- dir(extdata, pattern="^CEUtrio.*bgz$", full=TRUE)
-names(files) <- sub(".*_([0-9XY]+).*", "\\1", basename(files))
-seqinfo <- as(readRDS(file.path(extdata, "seqinfo.rds")), "Seqinfo")
-stack <- GenomicFiles::VcfStack(files, seqinfo)
-gr <- as(seqinfo(stack)[rownames(stack)], "GRanges")
-## RangedVcfStack
-rgstack <- GenomicFiles::RangedVcfStack(stack, rowRanges = gr)  
-rgstack
